f6631f6b7864704aed0e5b10243ab59e4e76e935
rosie <rosie@macbook-pro-77.ad.uvu.edu>
2024-01-18 11:11:55 -0700

    ok

--- Notes/Class Notes.R
+++ Notes/Class Notes.R
@@ -56,7 +56,11 @@ class(x)
 class(class(x))
 #things in quotes are not considered numerics
 
+<<<<<<< HEAD
 #Vector has a class (numeric, character, integer, data frame). Has one dimension. Everytjing inside must be the same class. Lenght of vector can equal 0
+=======
+#Vector has a class (numeric, character, integer, data frame,logical). Has one dimension. Everytjing inside must be the same class. Lenght of vector can equal 0
+>>>>>>> 761f226 (renamed notes and added new info)
 length(c())
 
 
@@ -74,10 +78,15 @@ letters[c(1,3,5)] #gives you first, third, and fifth letters
 a <-  1:10
 b <- 2:11
 c <- letters[1:10]
+<<<<<<< HEAD
+=======
+d <- rep(TRUE,10)
+>>>>>>> 761f226 (renamed notes and added new info)
 a
 b
 c
 
+<<<<<<< HEAD
 cbind(a,b,c)
 
 #dataframe
@@ -85,3 +94,52 @@ z <- data.frame(a,b,c) #recognized as data, only works if every item is the same
 class(z)
 length(z)
 dim(z) #gives dimension of object
+=======
+cbind(a,b,c,d)
+
+#dataframe
+z <- data.frame(a,b,c,d) #recognized as data, only works if every item is the same length
+class(z)
+length(z)
+dim(z) #gives dimension of object
+
+rep(TRUE,10) #rep() is used for replicating or repeating stuff
+
+#logical class vectors, used for true and false 
+1>0
+0>=0 #greater than or equal to
+3<0
+1==1 #equal to
+1 !=1 #not equal to
+5>a #tells you true or false for each value of a vector
+
+
+a[a<5]#only giving rows where a is less than 5 in 
+z[5>a,]
+
+#dataframes have 2 dimensions [row,column
+
+#pull out rows of z where column c is equal to "b"
+z[c=="b",]
+
+#rows of iris where Sepal.Length is greater than 5
+big_iris=iris[iris$Sepal.Length>5,]
+#how many rows had sepal length greater than 1
+dim(big_iris)[1]
+nrow(big_iris)
+#dataframe$ gives you access to all the columns in the dataframe
+
+
+big_iris$Sepal.Area=big_iris$Sepal.Length*big_iris$Sepal.Width #creates new column in big iris which is sepal area
+
+big_setosa <- big_iris[big_iris$Species=="setosa",]#giving the rows where the big iris species column is equal to setosa
+
+#mean sepal area from big_setosa
+mean(big_setosa$Sepal.Area)
+plot(big_setosa$Sepal.Length,big_setosa$Sepal.Width)
+cumsum(big_setosa$Sepal.Area)#cummulative sum
+cumprod(big_setosa$Sepal.Area)#cummulative product
+
+#give me all the rows in iris where the species is virginica or setosa
+iris[iris$Species==c("virginica","setosa"),]
+>>>>>>> 761f226 (renamed notes and added new info)
