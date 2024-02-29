9b32708c2e571be1a93b133787721739ab430233
rosie <rosie@dhcp-10-5-23-127.ad.uvu.edu>
2024-02-29 11:15:35 -0700

    update 02/29/2024

--- Notes/Class_Notes.R
+++ Notes/Class_Notes.R
@@ -888,9 +888,103 @@ mpg %>% ggplot(aes(x=displ,y=cty))+
 glm(data=mpg,
     formula=cty~displ) %>% summary()
 
-
-
+library(tidyverse)
+data.frame(A=rnorm(10,mean=0,sd=1),
+           B=rnorm(10,mean=5,sd=1)) %>% 
+  pivot_longer(everything()) %>% 
+  ggplot(aes(x=value,fill=name))+
+  geom_density()
   
 
 
 
+#02/29/24####
+
+#use mpg
+#build a model that predicts cty as a function of displ
+library(tidyverse)
+library(easystats)
+library(MASS)
+
+mod1 <- glm(data=mpg,formula=cty~displ)
+
+mod1$residuals #residuals is distance from predictor line for each point
+
+mod2 <-glm(data=mpg,formula=cty~displ+cyl)
+
+mod3 <- glm(data=mpg,formula=cty~displ*cyl)#influcence of displacement on city can depend on cylinder
+
+
+
+#add new column indicating whether automatic
+mpg <- 
+mpg %>% mutate(auto=grepl("auto",mpg$trans))
+
+mod4 <- glm(data=mpg,formula=cty~displ*cyl*auto)
+
+mod5 <- glm(data=mpg,formula=cty~displ+year+cyl*trans*drv*fl+class)
+formula(mod5)
+
+step <- stepAIC(mod5)##Removing variable in model if they aren't significant#####
+#step
+
+mod_best <- glm(data=mpg,formula=formula(step))
+
+summary(mod4)
+
+compare_models(mod1,mod2,mod3,mod4,mod_best)
+compare_performance(mod1,mod2,mod3,mod4,mod_best)#AIC smaller is better penalizes you for unimportant predictors USE WITH CAUTION####
+
+compare_performance(mod1,mod2,mod3,mod4,mod_best) %>% plot ##plotting performance of models####
+
+
+mpg$pred <- predict(mod1,mpg)
+mpg$pred2 <- predict(mod2,mpg)
+mpg$pred3 <- predict(mod3,mpg)
+mpg$predbest <- predict(mod_best,mpg)
+
+
+mpg %>% 
+  pivot_longer(starts_with("pred")) %>% ggplot(aes(x=displ,y=cty,color=factor(cyl)))+
+  geom_point()+
+  geom_point(aes(y=value),color='black')+
+  facet_wrap(~name)#plot coloring by cylinder factor, the black is looking at the predicted y values compared to the actual
+
+
+
+
+
+mpg %>% 
+  ggplot(aes(x=cty,y=pred))+
+  geom_point() #if modely was perfict you'd see a 1 to 1 straight line
+
+
+mpg %>% 
+  ggplot(aes(x=cty,y=pred2))+
+  geom_point()
+
+mpg %>% 
+  ggplot(aes(x=cty,y=pred3))+
+  geom_point()
+
+mpg %>% 
+  ggplot(aes(x=cty,y=predbest))+
+  geom_point()
+
+predict(mod1,data.frame(displ=1:100))#gave model 1 through 100 and pridicted cty mpge
+
+
+
+mpg %>% 
+  ggplot(aes(x=displ,y=cty))+
+  geom_smooth(method="glm") #mod1
+
+mpg %>% 
+  ggplot(aes(x=displ,y=cty,color=factor(cyl)))+
+  geom_smooth(method="glm")#mod3
+
+
+
+
+
+check_model(mod3)#tells if you model is meeting these assumptions####
